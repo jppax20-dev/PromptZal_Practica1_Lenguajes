2b3a579a57394dc174c978d1323233044304d4a0
Josue Oxlaj <jppax20@gmail.com>
2026-08-11 10:44:04 -0600

    Estructura inicial del proyecto

---
+++ src/main/java/com/cunoc/promptzal_analizadorlexico/PromptZAL_AnalizadorLexico.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.cunoc.promptzal_analizadorlexico;
+
+/**
+ *
+ * @author jppax
+ */
+public class PromptZAL_AnalizadorLexico {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
