f63eefe4c26a34c37b66bee1a0324bc59e4a7b5d
Josue Oxlaj <jppax20@gmail.com>
2026-08-15 21:34:14 -0600

    Clase Main lista para leer archivos

--- src/main/java/com/cunoc/promptzal_analizadorlexico/PromptZAL_AnalizadorLexico.java
+++ src/main/java/com/cunoc/promptzal_analizadorlexico/PromptZAL_AnalizadorLexico.java
@@ -7,6 +7,7 @@ import com.cunoc.promptzal_analizadorlexico.lexer.TipoToken;
 import com.cunoc.promptzal_analizadorlexico.lexer.Token;
 import com.cunoc.promptzal_analizadorlexico.lexer.Lexer;
 import com.cunoc.promptzal_analizadorlexico.reportes.ErrorLexico;
+import com.cunoc.promptzal_analizadorlexico.reportes.GenerarReporte;
 //herramientas 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -31,21 +32,55 @@ public class PromptZAL_AnalizadorLexico {
             return; // mensaje de error de leer archivo
         }
         
-        Lexer lexer = new lexer(codigo);
+        Lexer lexer = new Lexer(codigo);
         lexer.analizar();
         
-        list<Token> tokens = lexer.getTokens(); 
-        list<ErrorLexico> errores = Lexer.getEores();
+        List<Token> tokens = lexer.getTokens(); 
+        List<ErrorLexico> errores = lexer.getErrores();
         
         mostrarTablaConsola(tokens, errores);
         
         try {
-            GeneradorReporte generador = new GeneradorReporte();  
-            generador.generarReportetokesn (tokens, "Reporte_de_tokens.html");
+            GenerarReporte generador = new GenerarReporte();
+            generador.generarReporteTokens(tokens, "Reporte_de_tokens.html");
             generador.generarReporteErrores(errores, "Reporte_de_errores.html");
             System.out.println("Reportes Generados: 1.Reporte_de_tokens.html y 2.Reporte_de_Errores.html");
         } catch (IOException e ){ //manejo del error 
             System.out.println("Error al generar los reportes HTML:" + e.getMessage());
         }
     }
+    //usando Filereader y Buffer reader lee el archivo, si no encuentra nada regresa null 
+    private static String leerArchivo ( String ruta){
+        StringBuilder sb = new StringBuilder();
+        
+        try(BufferedReader br = new BufferedReader (new FileReader(ruta))){
+            String linea;
+            while ((linea = br.readLine())!=null){
+                sb.append(linea).append("\n");
+            }
+            
+        } catch (IOException e){
+            System.out.println( "No se pudo leer el archivo " + e.getMessage());
+            return null; 
+        }
+        return sb.toString();
+            
+        }
+         //muestra de resultados como una tabla 
+         private static void mostrarTablaConsola (List<Token> tokens, List<ErrorLexico> errores){
+             System.out.println("=== TOKENS ===");
+             for (Token t : tokens){
+                 System.out.println(t);
+             }
+             System.out.println("=== ERRORES LEXICOS ===");
+             if (errores.isEmpty()){
+                 System.out.println("No se encontraron errores.");
+             } else {
+                 for (ErrorLexico e : errores){
+                     System.out.println(e);
+                 }
+             }
+             
+         }
+    
 } // final 
